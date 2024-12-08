69d3c35c636dc54ee83977aad444261d1c296c96
Gabriel Garay <gabriel.garay@esolutions.com.ar>
2024-12-08 20:45:58 -0300

    fix test user service

--- test/user-service.spec.ts
+++ test/user-service.spec.ts
@@ -1,4 +1,5 @@
-import { TestingModule, Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '@src/Services/UserService';
 import { UserDao } from '@src/Daos/UserDao';
 import { UserProfileDao } from '@src/Daos/UserProfileDao';
 import { User } from '@src/Models/Entities/UserEntity';
@@ -7,7 +8,8 @@ import CreateUserRequest from '@src/Models/Request/User/CreateUserRequest';
 import UpdateUserRequest from '@src/Models/Request/User/UpdateUserRequest';
 import SuccessfulResponse from '@src/Models/Response/SuccessfulResponse';
 import { GetUserResponse } from '@src/Models/Response/User/GetUserResponse';
-import { UserService } from '@src/Services/UserService';
+import HttpCustomException from '@src/Exceptions/HttpCustomException';
+import { StatusCodeEnums } from '@src/Enums/StatusCodeEnums';
 
 describe('UserService', () => {
     let userService: UserService;
@@ -21,12 +23,12 @@ describe('UserService', () => {
                 {
                     provide: UserDao,
                     useValue: {
+                        findByEmail: jest.fn(),
                         create: jest.fn(),
+                        listUsers: jest.fn(),
+                        findById: jest.fn(),
                         update: jest.fn(),
                         deleteById: jest.fn(),
-                        findById: jest.fn(),
-                        findByEmail: jest.fn(),
-                        listUsers: jest.fn(),
                     },
                 },
                 {
@@ -47,50 +49,168 @@ describe('UserService', () => {
         expect(userService).toBeDefined();
     });
 
-    it('should create a user', async () => {
-        const createUserRequest: CreateUserRequest = { name: 'John', email: 'john@example.com', age: 30, profileCode: 'ADMIN' };
-        const userProfile: UserProfile = new UserProfile();
-        jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(userProfile);
-        jest.spyOn(userDao, 'findByEmail').mockResolvedValue(null);
-        jest.spyOn(userDao, 'create').mockResolvedValue(new User());
+    describe('createUser', () => {
+        it('should create a new user successfully', async () => {
+            const createUserRequest: CreateUserRequest = {
+                name: 'John',
+                email: 'john@example.com',
+                age: 30,
+                profileCode: 'ADMIN',
+            };
 
-        const result = await userService.createUser(createUserRequest);
-        expect(result).toEqual(new SuccessfulResponse('Usuario creado correctamente'));
+            const userProfile: UserProfile = new UserProfile();
+            jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(userProfile);
+            jest.spyOn(userDao, 'findByEmail').mockResolvedValue(null);
+            jest.spyOn(userDao, 'create').mockResolvedValue(new User());
+
+            const result = await userService.createUser(createUserRequest);
+            expect(result).toEqual(new SuccessfulResponse('Usuario creado correctamente'));
+        });
+
+        it('should throw an error if the user already exists', async () => {
+            const createUserRequest: CreateUserRequest = {
+                name: 'John',
+                email: 'john@example.com',
+                age: 30,
+                profileCode: 'ADMIN',
+            };
+
+            const existingUser: User = new User();
+            jest.spyOn(userDao, 'findByEmail').mockResolvedValue(existingUser);
+
+            await expect(userService.createUser(createUserRequest)).rejects.toThrow(
+                new HttpCustomException('Ya existe un usuario con este correo', StatusCodeEnums.USER_ALREADY_EXISTS),
+            );
+        });
+
+        it('should throw an error if the user profile does not exist', async () => {
+            const createUserRequest: CreateUserRequest = {
+                name: 'John',
+                email: 'john@example.com',
+                age: 30,
+                profileCode: 'ADMIN',
+            };
+
+            jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(null);
+            jest.spyOn(userDao, 'findByEmail').mockResolvedValue(null);
+
+            await expect(userService.createUser(createUserRequest)).rejects.toThrow(
+                new HttpCustomException('Perfil de usuario no encontrado', StatusCodeEnums.USER_PROFILE_NOT_FOUND),
+            );
+        });
     });
 
-    it('should list users', async () => {
-        const users: User[] = [new User()];
-        jest.spyOn(userDao, 'listUsers').mockResolvedValue(users);
+    describe('listUsers', () => {
+        it('should return a list of users', async () => {
+            const users: User[] = [new User()];
+            jest.spyOn(userDao, 'listUsers').mockResolvedValue(users);
 
-        const result = await userService.listUsers();
-        expect(result).toEqual(users);
+            const result = await userService.listUsers();
+            expect(result).toEqual(users.map((user) => new GetUserResponse(user)));
+        });
+
+        it('should throw an error if no users are found', async () => {
+            jest.spyOn(userDao, 'listUsers').mockResolvedValue([]);
+
+            await expect(userService.listUsers()).rejects.toThrow(
+                new HttpCustomException('No se encontraron usuarios', StatusCodeEnums.USER_NOT_FOUND),
+            );
+        });
     });
 
-    it('should find a user by id', async () => {
-        const user: User = new User();
-        jest.spyOn(userDao, 'findById').mockResolvedValue(user);
+    describe('findUserById', () => {
+        it('should return a user by id', async () => {
+            const user: User = new User();
+            jest.spyOn(userDao, 'findById').mockResolvedValue(user);
 
-        const result = await userService.findUserById(1);
-        expect(result).toEqual(new GetUserResponse(user));
+            const result = await userService.findUserById(1);
+            expect(result).toEqual(new GetUserResponse(user));
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            jest.spyOn(userDao, 'findById').mockResolvedValue(null);
+
+            await expect(userService.findUserById(1)).rejects.toThrow(
+                new HttpCustomException('Usuario no encontrado', StatusCodeEnums.USER_NOT_FOUND),
+            );
+        });
     });
 
-    it('should update a user by id', async () => {
-        const updateUserRequest: UpdateUserRequest = { name: 'John', email: 'john@example.com', age: 30, profileCode: 'ADMIN' };
-        const user: User = new User();
-        jest.spyOn(userDao, 'findById').mockResolvedValue(user);
-        jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(new UserProfile());
-        jest.spyOn(userDao, 'update').mockResolvedValue(user);
+    describe('updateUserById', () => {
+        it('should update a user by id successfully', async () => {
+            const updateUserRequest: UpdateUserRequest = {
+                name: 'John',
+                email: 'john@example.com',
+                age: 30,
+                profileCode: 'ADMIN',
+            };
 
-        const result = await userService.updateUserById(1, updateUserRequest);
-        expect(result).toEqual(new SuccessfulResponse('Usuario actualizado correctamente'));
+            const user: User = new User();
+            jest.spyOn(userDao, 'findById').mockResolvedValue(user);
+            jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(new UserProfile());
+            jest.spyOn(userDao, 'update').mockResolvedValue(user);
+
+            const result = await userService.updateUserById(1, updateUserRequest);
+            expect(result).toEqual(new SuccessfulResponse('Usuario actualizado correctamente'));
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            const updateUserRequest: UpdateUserRequest = {
+                name: 'John',
+                email: 'john@example.com',
+                age: 30,
+                profileCode: 'ADMIN',
+            };
+
+            jest.spyOn(userDao, 'findById').mockResolvedValue(null);
+
+            await expect(userService.updateUserById(1, updateUserRequest)).rejects.toThrow(
+                new HttpCustomException('Usuario no encontrado', StatusCodeEnums.USER_NOT_FOUND),
+            );
+        });
+
+        it('should throw an error if no fields are provided for update', async () => {
+            const updateUserRequest: UpdateUserRequest = {};
+
+            const user: User = new User();
+            jest.spyOn(userDao, 'findById').mockResolvedValue(user);
+
+            await expect(userService.updateUserById(1, updateUserRequest)).rejects.toThrow(
+                new HttpCustomException('Debe proporcionar al menos un campo para actualizar', StatusCodeEnums.INVALID_REQUEST),
+            );
+        });
+
+        it('should throw an error if the user profile does not exist', async () => {
+            const updateUserRequest: UpdateUserRequest = {
+                profileCode: 'ADMIN',
+            };
+
+            const user: User = new User();
+            jest.spyOn(userDao, 'findById').mockResolvedValue(user);
+            jest.spyOn(userProfileDao, 'findByCode').mockResolvedValue(null);
+
+            await expect(userService.updateUserById(1, updateUserRequest)).rejects.toThrow(
+                new HttpCustomException('Perfil de usuario no encontrado', StatusCodeEnums.USER_PROFILE_NOT_FOUND),
+            );
+        });
     });
 
-    it('should delete a user by id', async () => {
-        const user: User = new User();
-        jest.spyOn(userDao, 'findById').mockResolvedValue(user);
-        jest.spyOn(userDao, 'deleteById').mockResolvedValue();
+    describe('deleteUserById', () => {
+        it('should delete a user by id successfully', async () => {
+            const user: User = new User();
+            jest.spyOn(userDao, 'findById').mockResolvedValue(user);
+            jest.spyOn(userDao, 'deleteById').mockResolvedValue();
 
-        const result = await userService.deleteUserById(1);
-        expect(result).toEqual(new SuccessfulResponse('Usuario eliminado correctamente'));
+            const result = await userService.deleteUserById(1);
+            expect(result).toEqual(new SuccessfulResponse('Usuario eliminado correctamente'));
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            jest.spyOn(userDao, 'findById').mockResolvedValue(null);
+
+            await expect(userService.deleteUserById(1)).rejects.toThrow(
+                new HttpCustomException('Usuario no encontrado', StatusCodeEnums.USER_NOT_FOUND),
+            );
+        });
     });
 });
